8c4147e6bc6dbb7611bbde4d615e5baa6ed2977d
aco <>
2018-01-25 21:33:04 +0800

    test home.html

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
 var http = require('http');
 
+var express = require("express");
+var app = express();
+
+app.use('/RESOURCES',express.static(__dirname + '/RESOURCES'));
+
+var port2 = process.env.PORT || 5050;
+var server = app.listen(port2);
+
 var server = http.createServer(function(request, response) {
 
+    response.writeHead(200,{"Content-Type": "text/html"});
+    
+      fs.readFile('./RESOURCES/home.html', 'utf8', function(error, data) {
+        if (error) {
+          response.writeHead(404);
+          response.write('File not found!');
+        }
+        else {
+          response.write(data);
+        }
+        response.end();
+      });
+      /*
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.end("Hello World!");
-
+*/
 });
 
 var port = process.env.PORT || 1337;
